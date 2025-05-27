d473d623b46a95af187d96f18a7be46251bd6d44
narahim <79778428+skfkgla@users.noreply.github.com>
2025-05-27 14:55:23 +0900

    Time: 0 ms (100%), Space: 42.5 MB (66.87%) - LeetHub

---
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+// public class ListNode {
+//     int val;
+//     ListNode next;
+
+//     ListNode() {
+//     }
+
+//     ListNode(int val) {
+//         this.val = val;
+//     }
+
+//     ListNode(int val, ListNode next) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode reverseNode = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = reverseNode;
+            reverseNode = head;
+            head = nextNode;
+        }
+        return reverseNode;
+    }
+}
